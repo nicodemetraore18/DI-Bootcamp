7a6465a7f5bb34f16a84c60f43ff6bdc87c1d70a
NBA <nicodemetraore18@gmail.com>
2022-09-02 16:41:29 +0200

    little bit update

--- week2/Day4/DailyChallenge/script.js
+++ week2/Day4/DailyChallenge/script.js
@@ -1,5 +1,30 @@
 let words=prompt("Enter several words separated by commas");
-let array=[];
-array.push(words);
-array.splice(",")
-console.log(array);
+let liste=words.split(",");
+let max=0;
+for(let mot of liste ){
+	if (mot.length > max){
+		max=mot.length;
+	}
+
+}
+//Hello, World, in, a, frame
+
+max=max+4;
+let starsOrWords="";
+console.log(starsOrWords.padStart(max,"*"));
+
+for(let mot of liste){
+let reste=max-(2+mot.length)-1;
+
+
+	console.log("".padStart(2,"* ")+mot+"".padEnd(reste," ")+"".padEnd(1,"*"));
+}
+
+
+
+console.log(starsOrWords.padStart(max,"*"));
+
+
+
+
+
