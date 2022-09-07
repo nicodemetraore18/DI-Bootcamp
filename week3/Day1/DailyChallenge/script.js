04b6e27aea06e148e1c24210c7f62f3adf62a7f3
NBA <nicodemetraore18@gmail.com>
2022-09-07 14:23:58 +0200

    week3 day1 dailyChallenge done

--- week3/Day1/DailyChallenge/script.js
+++ week3/Day1/DailyChallenge/script.js
@@ -1,27 +1,34 @@
 let planets= [
      Mercure={nom:"Mercure",
-     	"couleur":"Peru"},
+     	nbrelune:2},
 
 	 Vénus={
 	 	nom:"Vénus",
+	 	nbrelune:2
 	 	},
 
 	 Terre={nom:"Terre",
+	 nbrelune:1
 	 	},
 
 	 Mars={nom:"Mars",
+	 nbrelune:2
 	 	} ,
 
 	 Jupiter={nom:"Jupiter",
+	 nbrelune:2
 	 	},
 
 	 Saturne={nom:"Saturne",
+	 nbrelune:2
 	 	},
 
 	 Uranus={nom:"Uranus",
+	 nbrelune:2
 	 	},
 
 	Neptune={nom:"Neptune",
+	nbrelune:2
 		} ]
 
  select= document.getElementsByTagName("section")[0];
@@ -29,15 +36,31 @@ let planets= [
 
 for (planet of planets)
 	{
-		
-		
+		let positionleft=5;
+		let positionbottom=40;
+		divparent=document.createElement('div');
+		divparent.classList.add("divpar");
  		newdiv=document.createElement('div');
  		newh1=document.createElement('h1')
  		newdiv.appendChild(newh1)
+ 		for(let i=0; i<planet.nbrelune; i++){
+ 		moon=document.createElement("div")	
+ 		moon.classList.add("moon");
+ 		divparent.appendChild(moon);
+ 		moon.style.left=positionleft+"px";
+ 		moon.style.top=positionbottom+"px";
+          positionleft+=50;
+          positionbottom+=140;
+
+ 		}
+ 		
+
+ 		
+ 		
  		text=document.createTextNode(planet.nom)
  		newh1.appendChild(text)
  		newdiv.classList.add("planet",planet.nom);
  		
- 		
-      select.appendChild(newdiv);
+ 		divparent.appendChild(newdiv);
+      select.appendChild(divparent);
  	}
